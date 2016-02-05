02e1accd27185d2b7fd9176206a1d52992e90168
ChadClark_Dev <chadclarke@bell.net>
2016-02-05 00:06:15 -0500

    New content for about section

--- Scripts/app.js
+++ Scripts/app.js
@@ -24,7 +24,7 @@ var aboutTitle;
     
       aboutParagraph = document.getElementById("aboutParagraph");
     
-    aboutParagraph.innerHTML = "Donec a laoreet urna, in dictum nisi. Etiam vestibulum neque quis ligula hendrerit, sed auctor ligula pretium. Praesent purus erat, consequat ac arcu vel, tempor vehicula eros. Maecenas eget congue dui, ac posuere quam. Quisque fringilla pretium nulla id tempor. Quisque hendrerit vitae augue sit amet elementum. Phasellus et nisi leo.Donec a laoreet urna, in dictum nisi. Etiam vestibulum neque quis ligula hendrerit, sed auctor ligula pretium. Praesent purus erat, consequat ac arcu vel, tempor vehicula eros. Maecenas eget congue dui, ac posuere quam. Quisque fringilla pretium nulla id tempor. Quisque hendrerit vitae augue sit amet elementum. Phasellus et nisi leo."
+    aboutParagraph.innerHTML = "Do you see that ipad floating to your right? Thats me on the far left and the others are my family. I grew up in a small town north of Toronto where simple living and family remain #1. I strarted working working with the web about a year ago and have been adicted ever since. Now adays i have been hand crafting clean, creative web sites for small buisnesses around the community. Using the latest tools to maipulate the web much like a blacksmith. I start with an idea and use tools such as HTML to build the foundation, PHP to menip[ulate, JAVASCRIPT for custom touches & CSS for the final touches. I have experince working with popular content management systmes such as WordPress, Shopify and OpenCart. I always try to be unique and continue on my jorney of becoming a talented web designer/developer. Check out my portfolio, if you like what you see please feel free to shoot me a message on the contact page."
 
 
 // Contact me h1
