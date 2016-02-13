a5bd02011c6fd9f9d9cbe964afa5ab2d3b368053
ChadClark_Dev <chadclarke@bell.net>
2016-02-12 23:36:16 -0500

    final update before submission

--- Scripts/app.js
+++ Scripts/app.js
@@ -21,9 +21,19 @@ var aboutTitle;
     
       aboutParagraph = document.getElementById("aboutParagraph");
     
-    aboutParagraph.innerHTML = "Do you see that ipad floating to your right? Thats me on the far left and the others are my family. I grew up in a small town north of Toronto where simple living and family remain #1. I strarted working working with the web about a year ago and have been adicted ever since. Now adays i have been hand crafting clean, creative web sites for small buisnesses around the community. Using the latest tools to maipulate the web much like a blacksmith. I start with an idea and use tools such as HTML to build the foundation, PHP to menip[ulate, JAVASCRIPT for custom touches & CSS for the final touches. I have experince working with popular content management systmes such as WordPress, Shopify and OpenCart. I always try to be unique and continue on my jorney of becoming a talented web designer/developer. Check out my portfolio, if you like what you see please feel free to shoot me a message on the contact page.";
+    aboutParagraph.innerHTML = "Do you see that ipad floating to your right? Thats me on the far left and the others are my family. I started working working with the web about a year ago and have been adicted ever since. Now a days i have been hand crafting clean, creative web sites for small buisnesses around the community. I have experince working with popular content management systmes such as WordPress, Shopify and OpenCart. I always try to be unique and continue on my jorney of becoming a talented web designer/developer. You can check out my portfolio below.";
 
 
+// header intro 
+
+var iam; 
+iam = document.getElementById("iam");
+iam.innerHTML = "I AM CHAD CLARKE";
+
+
+var ActionText;
+ActionText = document.getElementById("ActionText");
+ActionText.innerHTML = "Designer Developer Dreamer";
 
 
             // skills section 
